1026be0fa47ab2c83c857ba6d3da324af8410b3a
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2024-06-13 16:51:59 -0300

    Add files via upload

---
+++ amora.cs
@@ -0,0 +1,21 @@
+// See https://aka.ms/new-console-template for more information
+
+string resp;
+Console.WriteLine("A Thaina ama o Fulvio?");
+resp = Console.ReadLine();
+if(resp == "sim" || resp == "s" || resp == "muito" || resp =="mt" || resp == "uhum"){
+    Console.WriteLine("Võcê pode até dizer {0}, mas sabemos que não ama.", resp); 
+    }
+else
+{
+    Console.WriteLine("Tava mentindo esse tempo todo é? ");
+}
+Console.WriteLine("Mas eu te ");
+resp = "ama";
+Console.WriteLine("{0}{0}{0}{0} " + " {0}{0}  {0}{0} " + " {0}{0}{0}{0}", resp);
+Console.WriteLine("{0}      {0} " + " {0} {0}{0} {0} " + " {0}      {0}", resp);
+Console.WriteLine("{0}      {0} " + " {0}   {0}  {0} " + " {0}      {0}", resp);
+Console.WriteLine("{0}{0}{0}{0} " + " {0}        {0} " + " {0}      {0}", resp);
+Console.WriteLine("{0}      {0} " + " {0}        {0} " + " {0}      {0}", resp);
+Console.WriteLine("{0}      {0} " + " {0}        {0} " + " {0}      {0}", resp);
+Console.WriteLine("{0}      {0} " + " {0}        {0} " + " {0}{0}{0}{0}", resp);
